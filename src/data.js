fed7d305abc9d6d80330fc12edc73c84aaa748f8
roadblock37 <Roadblock37@gmail.com>
2022-10-04 09:59:50 -0400

    initial setup

---
+++ src/data.js
@@ -0,0 +1,124 @@
+const Data = [
+  {
+    id: 1,
+    title: "Work",
+    image: "./images/icon-work.svg",
+    color: "FF8B64",
+    timeframes: {
+      daily: {
+        current: 5,
+        previous: 7,
+      },
+      weekly: {
+        current: 32,
+        previous: 36,
+      },
+      monthly: {
+        current: 103,
+        previous: 128,
+      },
+    },
+  },
+  {
+    id: 2,
+    title: "Play",
+    image: "./images/icon-play.svg",
+    color: "55C2E6",
+    timeframes: {
+      daily: {
+        current: 1,
+        previous: 2,
+      },
+      weekly: {
+        current: 10,
+        previous: 8,
+      },
+      monthly: {
+        current: 23,
+        previous: 29,
+      },
+    },
+  },
+  {
+    id: 3,
+    title: "Study",
+    image: "./images/icon-study.svg",
+    color: "FF5E7D",
+    timeframes: {
+      daily: {
+        current: 0,
+        previous: 1,
+      },
+      weekly: {
+        current: 4,
+        previous: 7,
+      },
+      monthly: {
+        current: 13,
+        previous: 19,
+      },
+    },
+  },
+  {
+    id: 4,
+    title: "Exercise",
+    image: "./images/icon-exercise.svg",
+    color: "4BCF82",
+    timeframes: {
+      daily: {
+        current: 1,
+        previous: 1,
+      },
+      weekly: {
+        current: 4,
+        previous: 5,
+      },
+      monthly: {
+        current: 11,
+        previous: 18,
+      },
+    },
+  },
+  {
+    id: 5,
+    title: "Social",
+    image: "./images/icon-social.svg",
+    color: "7335D2",
+    timeframes: {
+      daily: {
+        current: 1,
+        previous: 3,
+      },
+      weekly: {
+        current: 5,
+        previous: 10,
+      },
+      monthly: {
+        current: 21,
+        previous: 23,
+      },
+    },
+  },
+  {
+    id: 6,
+    title: "Self Care",
+    image: "./images/icon-self-care.svg",
+    color: "F1C75B",
+    timeframes: {
+      daily: {
+        current: 0,
+        previous: 1,
+      },
+      weekly: {
+        current: 2,
+        previous: 2,
+      },
+      monthly: {
+        current: 7,
+        previous: 11,
+      },
+    },
+  },
+];
+
+export default Data;
