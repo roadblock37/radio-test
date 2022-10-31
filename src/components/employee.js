8b6abf04f33d0122196f1fe4e77c0834a26db6b7
roadblock37 <Roadblock37@gmail.com>
2022-10-31 14:58:17 -0400

    styled image

--- src/components/employee.js
+++ src/components/employee.js
@@ -1,18 +1,23 @@
 import ButtonContainer from "./buttonContainer";
 const Employee = () => {
-    return (
-        <div className="employee-container">
-            <section className="employee-card">
-          <img src="../images/image-jeremy.png" alt="employee"></img>
-          <div className="employee-info">
-            <p>Report for</p>
-            <h1>Jeremy Robson</h1>
-          </div>
-        </section>
-          <ButtonContainer/>
+  return (
+    <div className="employee-container">
+      <section className="employee-card">
+        <div className="img-border">
+          <img
+            src="../images/image-jeremy.png"
+            alt="employee"
+            height="78px"
+            width="78px"></img>
         </div>
-        
 
-    );
-}
-        export default Employee;
+        <div className="employee-info">
+          <p>Report for</p>
+          <h1>Jeremy Robson</h1>
+        </div>
+      </section>
+      <ButtonContainer />
+    </div>
+  );
+};
+export default Employee;
