23624e0d460488d9e1641d19bf78c5d82bbfa69d
roadblock37 <Roadblock37@gmail.com>
2022-10-31 14:49:51 -0400

    hover state for svg

--- src/components/button.js
+++ src/components/button.js
@@ -4,26 +4,25 @@ import { useDispatch, useSelector } from "react-redux";
 
 const Button = ({ id, filter }) => {
   const dispatch = useDispatch();
-  const {defaultTimeFrame} = useSelector((store) => store.hours);
+  const { defaultTimeFrame } = useSelector((store) => store.hours);
 
   return (
     <div>
-    <input
-      key={id}
-      name="filterGroup"
-      id={id}
-      value={filter}
-      type="radio"
-      // radio button will be check by default if the 
-      // defaultTimeFrame equals the filter name
-      checked={defaultTimeFrame === filter}
-      //   pass id into onClick to update timeframe state
-      onClick={() => {
-        dispatch(setTimeFrame(id));
-      }}></input>
+      <input
+        key={id}
+        name="filterGroup"
+        id={id}
+        value={filter}
+        type="radio"
+        // radio button will be check by default if the
+        // defaultTimeFrame equals the filter name
+        checked={defaultTimeFrame === filter}
+        //   pass id into onClick to update timeframe state
+        onChange={() => {
+          dispatch(setTimeFrame(id));
+        }}></input>
       <label for={filter}>{filter}</label>
     </div>
-    
   );
 };
 export default Button;
