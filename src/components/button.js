7cb8288eaedf9319dd9975b9d0dd0cbe3a5ef660
Paul Biber <roadblock37@gmail.com>
2022-10-31 14:10:38 -0400

    styled employee img

--- src/components/button.js
+++ src/components/button.js
@@ -6,26 +6,6 @@ const Button = ({ id, filter }) => {
   const dispatch = useDispatch();
   const {defaultTimeFrame} = useSelector((store) => store.hours);
 
-  // if (defaultTimeFrame === "weekly") {
-  //   return (
-  //     <div>
-  //       <label for={filter}>{filter}</label>
-  //     <input
-  //       key={id}
-  //       name="filterGroup"
-  //       id={id}
-  //       value={filter}
-  //       type="radio"
-  //       checked={true}
-  //       //   pass id into onClick to update timeframe state
-  //       onClick={() => {
-  //         dispatch(setTimeFrame(id));
-  //       }}></input>
-  //     </div>
-      
-  //   );
-  // }
-
   return (
     <div>
     <input
--- src/components/buttonContainer.js
+++ src/components/buttonContainer.js
@@ -1,9 +1,10 @@
 import Button from "./button";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector} from "react-redux";
 
 const ButtonContainer = () => {
   const { defaultFilter } = useSelector((store) => store.hours);
-
+/*map through the filter list 
+and render a button for each item*/
   return (
     <div className="filter-container">
       {defaultFilter.map((item) => {
